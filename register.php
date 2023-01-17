d8159dee347f4c0c9acb6c2ca148a9f05e645825
gustonecrush <codeboxezz@gmail.com>
2023-01-18 01:33:48 +0700

    convert to php

--- register.php
+++ register.php
@@ -28,6 +28,20 @@
         <img src="src/assets/logo.svg" class="logo-login" alt="logo" />
         <h1 class="login-title">Create New Account</h1>
         <form class="register-form" method="" action="">
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" class="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
           <input type="text" placeholder="username" name="username" />
           <input type="email" placeholder="email" name="email" />
           <input type="password" placeholder="password" name="password" />
@@ -43,10 +57,22 @@
           <button>Register</button>
           <p class="message">
             Have an account?
-            <a href="/web-olshop/login.html">Sign In</a>
+            <a href="/web-olshop/login.php">Sign In</a>
           </p>
         </form>
       </div>
     </div>
+
+    <!-- ====================== JS ====================== -->
+    <script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+      integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+      integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
